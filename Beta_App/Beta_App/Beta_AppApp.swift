e89ddbee381ab95b97d2242432ff3f8236a5cc7f
jezmoe <janrubido@gmail.com>
2024-10-21 20:25:38 -0500

    Initial Swift Files Upload

---
+++ Beta_App/Beta_App/Beta_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Beta_AppApp.swift
+//  Beta_App
+//
+//  Created by Jan Rubido on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Beta_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Beta_App/Beta_AppTests/Beta_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Beta_AppTests.swift
+//  Beta_AppTests
+//
+//  Created by Jan Rubido on 10/21/24.
+//
+
+import Testing
+@testable import Beta_App
+
+struct Beta_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
